345192360e60c1abc9c54573986e444881a49ca3
moisam <34644392+moisam@users.noreply.github.com>
2017-12-18 18:49:12 +0300

    Update PDFDocument.java

--- src/net/laylacp/pdflib/core/PDFDocument.java
+++ src/net/laylacp/pdflib/core/PDFDocument.java
@@ -1,3 +1,24 @@
+/*
+ *    Programmed By: Mohammed Isam [mohammed_isam1984@yahoo.com]
+ *    Copyright 2017 (c)
+ *
+ *    file: PDFDocument.java
+ *    This file is part of Layla-PDFLib.
+ *
+ *    Layla-PDFLib is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU Lesser General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    Layla-PDFLib is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public License
+ *    along with Layla-PDFLib.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package net.laylacp.pdflib.core;
 
 import net.laylacp.pdflib.core.types.ObjectId;
